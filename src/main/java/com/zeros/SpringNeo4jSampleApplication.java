fcc2b2e5de09a471d468b759b31da77e46fb2783
zeroual <abdellah.zeroual@jcdecaux.fr>
2016-02-10 17:55:09 +0100

    First commit

---
+++ src/main/java/com/zeros/SpringNeo4jSampleApplication.java
@@ -0,0 +1,12 @@
+package com.zeros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNeo4jSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNeo4jSampleApplication.class, args);
+	}
+}
